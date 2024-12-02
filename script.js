d503cc2fece96281707d0251e3d87fc99354abb5
Demox-bit <57812556+Demox-bit@users.noreply.github.com>
2024-12-02 15:15:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,62 @@
 
             const hiddenSection = document.getElementById('Hidden_place');
 
+
+            hiddenSection.style.display = hiddenSection.style.display === 'none' ? 'block' : 'none';
+
+            hiddenSection.scrollIntoView({ behavior: 'smooth' });
+
+        });
+
+    });
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        const span = document.getElementById('toggle-hidden2');
+
+        
+
+        span.addEventListener('click', function() {
+
+            const hiddenSection = document.getElementById('Hidden_place2');
+
+            hiddenSection.style.display = hiddenSection.style.display === 'none' ? 'block' : 'none';
+
+            hiddenSection.scrollIntoView({ behavior: 'smooth' });
+
+        });
+
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        const span = document.getElementById('toggle-hidden3');
+
+        
+
+        span.addEventListener('click', function() {
+
+            const hiddenSection = document.getElementById('Hidden_place2');
+
+            hiddenSection.style.display = hiddenSection.style.display === 'none' ? 'block' : 'none';
+
+            hiddenSection.scrollIntoView({ behavior: 'smooth' });
+
+        });
+
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        const span = document.getElementById('toggle-hidden9');
+
+        
+
+        span.addEventListener('click', function() {
+
+            const hiddenSection = document.getElementById('Hidden_place9');
+
             hiddenSection.style.display = hiddenSection.style.display === 'none' ? 'block' : 'none';
 
             hiddenSection.scrollIntoView({ behavior: 'smooth' });
